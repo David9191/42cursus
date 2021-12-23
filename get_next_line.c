a1408f0f202627d85133eee654feaf455d611880
David9191 <jisung9105@gmail.com>
2021-12-23 22:21:28 +0900

    design logic

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jislim <jislim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 02:01:47 by jislim            #+#    #+#             */
-/*   Updated: 2021/12/21 22:43:13 by jislim           ###   ########.fr       */
+/*   Updated: 2021/12/23 20:35:13 by jislim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,22 @@
 char	*get_next_line(int fd)
 {
 	static char	*backup;
-	char		buf[BUFFER_SIZE];
-	int			check_read;
+	char		*buf;
+	size_t		check_read;
 
+	if (fd < 0 || BUFFER_SIZE < 0)
+		return (NULL);
 	while (1)
 	{
 		check_read = read(fd, backup, BUFFER_SIZE);
+		if (check_read < 0)
+			return (NULL);
 		if (ft_strchr(backup, '\n'))
 		{
-			split;
+			// split;
 			break ;
 		}
 		else
 			continue ;
 	}
-
 }
