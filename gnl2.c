f3dc817e4c0dd1dbbe3dd40d9ae5feecbd325a56
David9191 <jisung9105@gmail.com>
2022-02-18 02:07:50 +0900

    main function good

--- gnl2.c
+++ gnl2.c
@@ -6,10 +6,28 @@
 /*   By: jislim <jislim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 21:11:31 by jislim            #+#    #+#             */
-/*   Updated: 2022/02/17 21:12:16 by jislim           ###   ########.fr       */
+/*   Updated: 2022/02/18 02:07:09 by jislim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "gnl2.h"
 
-char *get_next_line(int fd);
+char *read_save(int fd, char *backup)
+{
+	// 50번 따라 치기!!
+}
+
+char *get_next_line(int fd)
+{
+	static char	*backup;
+	char		*buf;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	backup = read_save(fd, backup);
+	if (!backup)
+		return (NULL);
+	buf = get_line(backup);
+	backup = save(backup);
+	return (backup);
+}
