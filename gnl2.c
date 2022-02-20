7778b2368368226182273ebaa52640f6e51dc076
David9191 <jisung9105@gmail.com>
2022-02-20 23:52:32 +0900

    utils

--- gnl2.c
+++ gnl2.c
@@ -6,7 +6,7 @@
 /*   By: jislim <jislim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 21:11:31 by jislim            #+#    #+#             */
-/*   Updated: 2022/02/19 23:51:53 by jislim           ###   ########.fr       */
+/*   Updated: 2022/02/20 00:11:35 by jislim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ char *read_save(int fd, char *backup)
 {
 	// 50번 따라 치기!!
 	// 해보자!
+	// 할 수 있다!
 }
 
 char *get_next_line(int fd)
