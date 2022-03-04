4f8acb760e52224009c690dd683ac539b9a05162
David9191 <jisung9105@gmail.com>
2022-03-04 16:29:48 +0900

    norminette header

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jislim <jisung9105@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 02:01:38 by jislim            #+#    #+#             */
-/*   Updated: 2022/03/04 16:08:36 by jislim           ###   ########.fr       */
+/*   Updated: 2022/03/04 16:29:39 by jislim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@
 #  define BUFFER_SIZE 42
 # endif
 
-char    *get_next_line(int fd);
+char	*get_next_line(int fd);
 size_t	ft_strlen(char *s);
 size_t	ft_strchr(char *s, int c);
 char	*ft_strjoin(char *dest, char *src);
