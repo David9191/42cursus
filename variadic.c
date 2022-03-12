cc317b8d00f4ec995623b5c26d7860a1f6cdc170
David9191 <jisung9105@gmail.com>
2022-03-12 23:35:23 +0900

    study stdarg

--- variadic.c
+++ variadic.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
-
+#include <stdarg.h>
 // int printf(const char * __restrict, ...) __printflike(1, 2);
 int ft_printf(const char *, ...);
 
-int main(void)
+void	printfNum(int args, ...)
 {
-	printf();
+	va_list ap;
+	int		num;
+
+	va_start(ap, args);
+	for (int i = 0; i < args; i++)
+	{
+		num = va_arg(ap, int);
+
+		printf("%d ", num);
+	}
+	va_end(ap);
+	printf("\n");
 }
 
-int
+int main(void)
+{
+	printfNum(1, 10);
+	printfNum(2, 10, 20);
+	printfNum(3, 10, 20, 30);
+	printfNum(4, 10, 20, 30, 40);
+
+	return (0);
+}
