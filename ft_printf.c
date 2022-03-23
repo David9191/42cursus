57e2ef94d241cb253151f2af4c6c9f98ac55552a
David9191 <jisung9105@gmail.com>
2022-03-23 23:22:02 +0900

    Today, I learned about printf

--- ft_printf.c
+++ ft_printf.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
+#include <unistd.h>
 
 // cspdiuxX%
 
 int	ft_printf(const char *format, ...)
 {
+	int	idx;
+	int	*p_idx = &idx;
+
 	printf("%s\n", format);
-	return (1);
+	printf("%%\n");
+	printf("%n", p_idx); // prints nothing / just wonder
+	printf("%4$s %3$d %1$d %2$d", 10, 20, 30, "fourth subsequent argument");
+	// optional filed. notation n$ means n번째 후속인자를 선택할거다.
+
+	// printf("hello \0 world\0"); error
+	return (0);
 }
 
 int	main(void)
 {
-	ft_printf("Hello world");
+	char a = 'A';
+	int b = 10;
+
+	printf("%c\n", a + b);
+	printf("%d\n", a + b);
+	ft_printf("Hello world!");
 	return (0);
 }
