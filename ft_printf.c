04e6015191badfca32c7270c6a20f812e5f3c3ea
David9191 <jisung9105@gmail.com>
2022-03-24 21:19:54 +0900

    2022.03.24-21:19

--- ft_printf.c
+++ ft_printf.c
@@ -20,19 +20,22 @@ int	ft_printf(const char *format, ...)
 
 int	main(void)
 {
-	char a = 'A';
-	int b = 10;
-	int c = 12;
-	unsigned int d = 0;
-	write(1, &d, 1);
+	// char a = 'A';
+	// int b = 10;
+	// int c = 12;
 
-	printf("%c\n", a + b);
-	printf("%d\n", a + b);
-	printf("%d\n", c);
-	printf("%i\n", -1);
-	printf("%u\n", 0);
-	printf("%u\n", -2);
-	printf("%u\n", 2147483647);
-	ft_printf("Hello world!");
+	// printf("%c\n", a + b);
+	// printf("%d\n", 2147483648);
+	// printf("%i\n", 2147483648);
+	// printf("%d\n", c);
+	// printf("%i\n", -1);
+	// printf("%u\n", 0);
+	printf("%u\n", -2147483647);
+	unsigned int a = -2147483647;
+	write(1, &a, 8);
+	write(1, "good", 4);
+	// printf("%u\n", 2147483647);
+	// printf("%lu %lu\n", sizeof(int), sizeof(va_list[1]));
+	// ft_printf("Hello world!");
 	return (0);
 }
