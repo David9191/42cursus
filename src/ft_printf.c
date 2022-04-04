439caee7f5407e85630b55aab07f9cb65b06e2f0
David9191 <jisung9105@gmail.com>
2022-04-04 20:42:34 +0900

    erase comment

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: jislim <jislim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 14:32:19 by jislim            #+#    #+#             */
-/*   Updated: 2022/04/04 20:01:02 by jislim           ###   ########.fr       */
+/*   Updated: 2022/04/04 20:42:12 by jislim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,20 +66,3 @@ int	ft_printf(const char *format, ...)
 	va_end(ap);
 	return (cnt_print);
 }
-
-// int	main(void)
-// {
-// 	int	a = 2147483647;
-// 	// ft_printf("%%\n%c\n%c\n", '%', '-');
-// 	// ft_printf("%% %c\n %s\t %p\n %d %i\n %x\n %X\n %u\n\n",
-// 	// '-', "hello world!", &a, a, a, 56789, 56789, -1);
-// 	ft_printf("%%%cft_print\n-----------\n%s\t%p\n%d	%i\n%s\n%x\n%X\n%u\n\n",
-// 	'-', "hello world!", "hello world!", a, a, "개행 굿", 56789, 56789, -1);
-// 	printf("%%%cprint\n-----------\n%s\t%p\n%d	%i\n%s\n%x\n%X\n%u",
-// 	'-', "hello world!", "hello world!", a, a, "개행 굿", 56789, 56789, -1);
-// 	// ft_printf("%d", 2147483648); 이 경우 어떻게 할거임? 고려하지마? 다른 분은 이 경우 뭐 나오는지 물어봐야겠다.
-// 	// -2147483648이 나왔다. 나도 그냥 하면 될 거 같다. 고려 안해도 됨. 위의 일자로부터 2일 후.
-// 	// 뭔가 ap를 들고 다니면 에러가 나는 것 같다...
-// 	// nono 들고 다녀도 됨.
-// 	return (0);
-// }
