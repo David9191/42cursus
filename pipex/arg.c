2fd72aef3a49b04e2561f7ed7af3216f64b34ffc
David9191 <jisung9105@gmail.com>
2022-06-13 13:40:09 +0900

    practice exec function

---
+++ pipex/arg.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int	main(int argc, char **argv, char **envp)
+{
+	int		n;
+	char	**env;
+
+	("\n--> In arg.c Main\n");
+	printf("argc = %d\n", argc);
+
+	for ( n = 0; n < argc; n++)
+		printf("argv[%d] = %s\n", n, argv[n]);
+	
+	env = envp;
+	while (*env)
+	{
+		printf("env = %s\n", *env);
+		env++;
+	}
+}
