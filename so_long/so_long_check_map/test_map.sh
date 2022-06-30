5e9699ae4941226e4c84f3657041de17e1c6ae68
David9191 <jisung9105@gmail.com>
2022-06-30 19:34:23 +0900

    success so_long

--- so_long/so_long_check_map/test_map.sh
+++ so_long/so_long_check_map/test_map.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-echo "check test_map_wall.ber"
+echo ">>check test_map_wall.ber"
 ../so_long test_map_wall.ber
 echo ""
 echo ""
 
-echo "check test_map_c.ber"
+echo ">>check test_map_c.ber"
 ../so_long test_map_c.ber
 echo ""
 echo ""
 
-echo "check test_map_e.ber"
+echo ">>check test_map_e.ber"
 ../so_long test_map_e.ber
 echo ""
 echo ""
 
-echo "check test_map_p.ber"
+echo ">>check test_map_p.ber"
 ../so_long test_map_p.ber
 echo ""
 echo ""
 
-echo "check test_map_rectangle.ber"
+echo ">>check test_map_rectangle.ber"
 ../so_long test_map_rectangle.ber
 echo ""
 echo ""
 
-echo "check test_map_invalid.ber"
+echo ">>check test_map_invalid.ber"
 ../so_long test_map_invalid.ber
 echo ""
