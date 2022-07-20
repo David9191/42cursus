88a240451c588e3609c0b4a7007c374b7150c992
David9191 <jisung9105@gmail.com>
2022-07-20 12:08:47 +0900

    update brew.sh

--- 3circle/minishell/brew.sh
+++ 3circle/minishell/brew.sh
@@ -4,8 +4,15 @@ cd ~/goinfre
 
 mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
 
+cd ~/goinfre
+
 eval "$(homebrew/bin/brew shellenv)"
 brew update --force --quiet
 chmod -R go-w "$(brew --prefix)/share/zsh"
 
 export PATH=$HOME/goinfre/homebrew/bin:$PATH
+
+cd ~/goinfre
+
+brew install readline
+brew info readline
